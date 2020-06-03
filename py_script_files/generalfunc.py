Changed computation of presssure gradients of tides; Added errors for 6 tidal frequencies; and many minor errors fixed

py_script_files/generalfunc.py
@@ -11,6 +11,7 @@ import math
 import shutil
 import os
 import settings
+import logging
 deg2rad= np.pi/180.0
 # A conversion function to convert the latitudes and longitudes to meteres.
 # note that this is a simple conversion formula and not very accurate
@@ -187,14 +188,24 @@ def FFT_signal(y,N,ts):
     return(Y_plot,fvec,tvec)
 
 
-def TidCorio_comput(tvec,M2,deg):  
+def TidCorio_comput(tvec,tide,Cordeg): 
+    deg1=Cordeg['deg1'];deg2=Cordeg['deg2'] 
     #coriolis frequ
-    phi=np.deg2rad(deg)
-    coriolis=(2*2*np.pi*np.sin(phi))/(86164.09/3600)
-    cori_period=2*np.pi/coriolis
-    arg_m2 = (np.abs(tvec-M2)).argmin()
-    arg_cor=(np.abs(tvec-cori_period)).argmin()
-    return(arg_m2,arg_cor,cori_period)
+    phi1=np.deg2rad(deg1);phi2=np.deg2rad(deg2)
+    coriolis=(2*2*np.pi)/(86164.09/3600)
+    corperd1=2*np.pi/(coriolis*np.sin(phi1))
+    corperd2=2*np.pi/(coriolis*np.sin(phi2))
+    arg_deg1=(np.abs(tvec-corperd1)).argmin()
+    arg_deg2=(np.abs(tvec-corperd2)).argmin()
+    arg_cor={'argdeg1':arg_deg1,'argdeg2':arg_deg2}
+    arg_m2 = (np.abs(tvec-tide['M2'])).argmin()
+    arg_s2 = (np.abs(tvec-tide['S2'])).argmin()
+    arg_mu2 = (np.abs(tvec-tide['MU2'])).argmin()
+    arg_o1 = (np.abs(tvec-tide['O1'])).argmin()
+    arg_k1 = (np.abs(tvec-tide['K1'])).argmin()
+    arg_m4 = (np.abs(tvec-tide['M4'])).argmin()
+    arg_tide={'M2':arg_m2,'S2':arg_s2,'MU2':arg_mu2,'O1':arg_o1,'K1':arg_k1,'M4':arg_m4}
+    return(arg_tide,arg_cor)
 
 def LPfilter (numtaps,p_x):     
     window=signal.hamming(numtaps)
@@ -233,12 +244,11 @@ def errstats(Xobs,Yobs,Xsim,Ysim,tmplierinv):
  werr=[werrx,werry,werra]
  return(merr,rms,werr)
 
-def Cordesfunc(Cor):
+def Cordesfunc(Cor,trate,hs):
   #this function gives an output string array
   #which describes the forces and parameters of the simulations.
-  s=settings.settings()
   f='Yes' if Cor[0]==1 else 'No' #Coriolis 
-  h=str(Cor[1])+"_"+str(s['trate']) if Cor[1]=='v' else str(Cor[1]) #ice thickness
+  h=str(hs)+str(Cor[1])+"_"+str(trate) if Cor[1]=='v' else str(Cor[1]) #ice thickness
   Uax='Yes' if Cor[2]==1 else 'No' #wind stresss
   Uay=Uax
   Utx='Yes' if Cor[4]==1 else 'No' #Tidal stress
@@ -247,16 +257,16 @@ def Cordesfunc(Cor):
   Uoy=Uox
   Pgxo='Yes' if Cor[8]==1 else 'No' #pressure gradients
   Pgyo=Pgxo
-  Pgxt='Yes' if Cor[10]==1 else 'No' #pressure gradients
+  Pgxt='Yes' if Cor[10]==1 else 'No' #pressure gradients tides
   Pgyt=Pgxt
   Cornam=[f,h,Uax,Uay,Utx,Uty,Uox,Uoy,Pgxo,Pgyo,Pgxt,Pgyt]
   #Folder name for storing the data.
   folname='h'+str(Cor[1])+'f'+str(Cor[0])+ \
           'A'+str(Cor[2])+'T'+str(Cor[4])+ \
-          'O'+str(Cor[6])+'P'+str(Cor[8]) if Cor[1]!='v' else \
-          'h'+str(Cor[1])+str(s['trate'])[1:4]+'f'+str(Cor[0])+ \
+          'O'+str(Cor[6])+'Po'+str(Cor[8])+'Pt'+str(Cor[10]) if Cor[1]!='v' else \
+          'h'+str(hs)+str(Cor[1])+str(trate)[1:4]+'f'+str(Cor[0])+ \
           'A'+str(Cor[2])+'T'+str(Cor[4])+ \
-          'O'+str(Cor[6])+'P'+str(Cor[8])
+          'O'+str(Cor[6])+'Po'+str(Cor[8])+'Pt'+str(Cor[10])
   return(Cornam,folname)
 
 #function to copy the log file to the folder of simulations.
@@ -276,19 +286,29 @@ def FTremMD(numtaps,Xib,Xis,tmplierinv):
   [xsres,fvec,tvec]=FFT_signal(Xsres,Nft,dt)
   tvec=tvec/3600 #Period in hours
   #computation of tidal and coriolis frequency arguments
-  M2=12.421 #M2 tidal frequency period
-  deg=74.7;deg1=79.  #latitude for coriolis
-  [arg_m2,arg_74,cori_period]= TidCorio_comput(tvec,M2,deg)
-  [arg_m2,arg_79,cori_period1]= TidCorio_comput(tvec,M2,deg1)
-  return(tvec,xbres,xsres,arg_m2,arg_74,arg_79)
+  tide={'M2':12.421,'S2':12.,'MU2':12.871,'O1':25.819,'K1':23.934,'M4':6.2103}
+  Cordeg={'deg1':74.7,'deg2':79}  #latitude for coriolis
+  [arg_tide,arg_cor]= TidCorio_comput(tvec,tide,Cordeg)
+  errftvec=errorFT(xbres,xsres,arg_tide)
+  return(tvec,xbres,xsres,arg_tide,arg_cor,errftvec)
+
+def errorFT(xbres,xsres,arg_tide):
+  errM2=xbres[arg_tide['M2']]
+  errS2=xbres[arg_tide['S2']]
+  errMU2=xbres[arg_tide['MU2']]
+  errO1=xbres[arg_tide['O1']]
+  errK1=xbres[arg_tide['K1']]
+  errM4=xbres[arg_tide['M4']]
+  errftvec=[errM2,errS2,errMU2,errO1,errK1,errM4]
+  return(errftvec)
 
 def thinrate(ho,trate):
   dtobs=15*60
   hn=ho+trate*dtobs
-  if hn<0.1:
+  if hn<0.05:
     logging.info("Error: Ice thickness decreasing to less than 0.1 ")
     logging.info("Using constant ice thickness of 0.1m")
-    hn=0.1
+    hn=0.05
   return(hn)
 def  main(): # doesn't work anymore 
  Tib=[2,3,4]

py_script_files/icemodel.py
@@ -79,6 +79,7 @@ def modelFx(x,consts,s):
  rho_air=s['rho_air']
  rho_ice=s['rho_ice']
  [f, h, Ua, Va, Ut, Vt, Uo, Vo,Pgx,Pgy,Pgxt,Pgyt] =consts
+ # print("h="+str(consts[1]))
  [xc,yc,u,v] = x
  Cwi=s['iCw'];Cai=s['iCa']
  #transformation for rotation of Ua
@@ -103,9 +104,9 @@ def modelFx(x,consts,s):
  Fx=np.zeros(N)
  Fx[0]=u;Fx[1]=v
  Fx[2]=((Cwi*rho_water*Uw*u_mag + 
-         Cai*rho_air*Ua*Ua_mag)/(rho_ice*h)+((f*v)))-g*Pgx # -g*Pgxt
+         Cai*rho_air*Ua*Ua_mag)/(rho_ice*h)+((f*v)))-g*Pgx -g*Pgxt
  Fx[3]=((Cwi*rho_water*Vw*u_mag+
-         Cai*rho_air*Va*Ua_mag)/(rho_ice*h)-((f*u)))-g*Pgy #-g*Pgyt
+         Cai*rho_air*Va*Ua_mag)/(rho_ice*h)-((f*u)))-g*Pgy -g*Pgyt
  #linear shear stress model
  # Fx[2]=((Cwi*rho_water*Uw +
  #         Cai*rho_air*Ua)/(rho_ice*h)+((f*v))) -g*Pgx

py_script_files/icesimulate.py
@@ -53,8 +53,8 @@ def simulate(s,Bnum,indexing,Cor):
   # h=0.5
   #ice thickness with thinning rate (trate) trate=0 for constat thickness 
   if ((ti*tmplier)%1==0):
-    hvec=np.append(hvec,ho)
-    h=gf.thinrate(ho,trate);ho=h
+    h=gf.thinrate(ho,trate)
+    hvec=np.append(hvec,h*Cor[1]);ho=h
   consts=[f,h,Ua, Va, Ut, Vt, Uo, Vo,Pgx,Pgy,Pgxt,Pgyt]
   consts=np.multiply(Cor,consts)
   if (mod=='ExplicitEuler'):
@@ -85,12 +85,13 @@ def body(Bnum,indexing,numtaps,Cor):
   prefix="BUOY_"
   bname=prefix+Bnum
   path = "../../generated_data/"+bname
-  (Cornam,folname)=gf.Cordesfunc(Cor)
+  s=settings.settings()
+  h=s['h'];trate=s['trate']
+  (Cornam,folname)=gf.Cordesfunc(Cor,trate,h)
   path=path+'/'+folname
   gf.mkdir_p(path)
   fedge=int(numtaps/2)
   logging.info("Model simulation started.")
-  s=settings.settings()
   Corn=[]
   if Cor[1]!='v':
     Corn=Cor
@@ -132,15 +133,16 @@ def body(Bnum,indexing,numtaps,Cor):
   errvel=np.column_stack((merr,rms,werr))
   # Fourier Transforms  
   # Longitude
-  (tvec,xbres,xsres,arg_m2,arg_74,arg_79)=gf.FTremMD(numtaps,Xib,Xis,tmplierinv)
-  gp.pltFT(path,"Longitude",xbres,xsres,tvec,arg_m2,arg_74,arg_79)
-  errlon_FT=xbres[arg_m2]-xsres[arg_m2]
+  (tvec,xbres,xsres,arg_tide,arg_cor,errftveclon)=gf.FTremMD(numtaps,Xib,Xis,tmplierinv)
+  gp.pltFT(path,"Longitude",xbres,xsres,tvec,arg_tide,arg_cor)
+  # error computations
+
   # Latitude
-  (tvec,ybres,ysres,arg_m2,arg_74,arg_79)=gf.FTremMD(numtaps,Yib,Yis,tmplierinv)
-  gp.pltFT(path,"Latitude",ybres,ysres,tvec,arg_m2,arg_74,arg_79)
+  (tvec,ybres,ysres,arg_tide,arg_cor,errftveclat)=gf.FTremMD(numtaps,Yib,Yis,tmplierinv)
+  gp.pltFT(path,"Latitude",ybres,ysres,tvec,arg_tide,arg_cor)
   logging.info("Fourier Transforms plotted.")
-  errlat_FT=ybres[arg_m2]-ysres[arg_m2]
-  err_ft=[errlon_FT,errlat_FT]
+  err_ft=np.row_stack((errftveclon,errftveclat))
+
   #creating excel file
   simpost2excel(path,bname,Xis,Yis,hvec,Cornam,errpos,errvel,err_ft)
   logging.info("Excel data file created for simulated data.")
@@ -169,8 +171,9 @@ def simpost2excel(path,bname,Xis,Yis,hvec,Cornam,errpos,errvel,err_ft):
  dfevx=pd.DataFrame({'x': errvel[0,:]})
  dfevy=pd.DataFrame({'y': errvel[1,:]})
  dfeva=pd.DataFrame({'absolute': errvel[2,:]})
- dfeft=pd.DataFrame({'Error FT': ['Longitude','Latitude']})
- dfeftv=pd.DataFrame({'Value': err_ft})
+ dfeft=pd.DataFrame({'Error_tide': ['M2','S2','MU2','O1','K1','M4']})
+ dfeftlo=pd.DataFrame({'Longitude': err_ft[0,:]})
+ dfeftla=pd.DataFrame({'Latitude': err_ft[1,:]})
  dfb.to_excel(writer,'Sheet1',startcol=0,startrow=0,index=False)
  dfxis.to_excel(writer,'Sheet1', startcol=0,startrow=2,index=False)
  dfyis.to_excel(writer,'Sheet1', startcol=1,startrow=2,index=False)
@@ -187,7 +190,8 @@ def simpost2excel(path,bname,Xis,Yis,hvec,Cornam,errpos,errvel,err_ft):
  dfevy.to_excel(writer,'Sheet1',startcol=11,startrow=9,index=False)
  dfeva.to_excel(writer,'Sheet1',startcol=12,startrow=9,index=False)
  dfeft.to_excel(writer,'Sheet1',startcol=9,startrow=14,index=False)
- dfeftv.to_excel(writer,'Sheet1',startcol=10,startrow=14,index=False)
+ dfeftlo.to_excel(writer,'Sheet1',startcol=10,startrow=14,index=False)
+ dfeftla.to_excel(writer,'Sheet1',startcol=11,startrow=14,index=False)
  workbook  = writer.book
  worksheet = writer.sheets['Sheet1']
  merge_format = workbook.add_format({

py_script_files/processdata.py
@@ -50,7 +50,7 @@ def processing(Bnum,indexing,TD,AD,OD,ID,numtaps,sdate):
     #interpolating the u and v velocities to the buoy locations.
     Buoy_Xi=Xib #buoy longitude in hourly basis (x position)
     Buoy_Yi=Yib
-    Ut=[];Vt=[];SSHt=[]
+    Ut=[];Vt=[];Pgxt=[];Pgyt=[]
     for i in range(arrlen):
         ut_time=ut[i,:];vt_time=vt[i,:];ssht_time=ssht[i,:]
         xk=Buoy_Xi[i];yk=Buoy_Yi[i]
@@ -72,30 +72,14 @@ def processing(Bnum,indexing,TD,AD,OD,ID,numtaps,sdate):
         #creating tides at buoy locations
         Ut=np.append(Ut,uinterp(Buoy_Xi[i],Buoy_Yi[i]))
         Vt=np.append(Vt,vinterp(Buoy_Xi[i],Buoy_Yi[i]))
-        SSHt=np.append(SSHt,sshinterp(Buoy_Xi[i],Buoy_Yi[i]))
-    # computing the ssh gradients on the buoy locations. 
-    logging.info("Computing ssh gradients for tidal sshs.")
-    Pgxt=[];Pgyt=[];dlatvec=dlonvec=dzettvec=[]
-    Pgxt=np.append(Pgxt,0);Pgyt=np.append(Pgyt,0)
-    for i in range(arrlen-1):
-        dzett=SSHt[i+1]-SSHt[i]
-        dzettvec=np.append(dzettvec,dzett)
-        lat=Buoy_Yi[i]
-        dlat=Buoy_Yi[i+1]-Buoy_Yi[i]
-        dlatvec=np.append(dlatvec,dlat)
-        dlon=Buoy_Xi[i+1]-Buoy_Xi[i]
-        dlonvec=np.append(dlonvec,dlon)
+        #computation of gradient
+        lat=Buoy_Yi[i];dlon=0.02;dlat=0.01
+        dzx=sshinterp(Buoy_Xi[i]+dlon,Buoy_Yi[i])-sshinterp(Buoy_Xi[i],Buoy_Yi[i])
+        dzy=sshinterp(Buoy_Xi[i],Buoy_Yi[i]+dlat)-sshinterp(Buoy_Xi[i],Buoy_Yi[i])
         (dy,dx)=gf.latlon2meters(lat,dlat,dlon)
-        dy1=abs(dy);dx1=abs(dx)
-        if dy1<=1e-25 and dx1>=1e-25 :
-            dzdy=0;dzdx=dzett/dx
-        elif dy1>=1e-25 and dx1<=1e-25 :
-            dzdy=dzett/dy;dzdx=0
-        elif dy1<=1e-25 and dx1<=1e-25 :
-            dy=0;dzdx=0
-        else:
-            dzdx=dzett/dx;dzdy=dzett/dy
+        dzdx=dzx/dx;dzdy=dzy/dy
         Pgxt=np.append(Pgxt,dzdx);Pgyt=np.append(Pgyt,dzdy)
+
     logging.info("Tidal ssh gradients computation done.")        
     logging.info("Interpolation successfully done. Processing complete for GTSM data.")
    